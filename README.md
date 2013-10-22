6d486be55799bc5d0697546f486f82f9d6da327b
Corey McMahon <contact@coreymcmahon.com>
2013-10-22 14:11:42 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+ubuntu-nginx-setup
+==================
+
+Script to bootstrap a clean EC2 Ubuntu 13.x to a state where it can act as a webserver using Nginx
+
---
+++ setup.sh
@@ -0,0 +1,16 @@
+# Misc.
+sudo apt-get -y install git
+
+# Install MySQL
+sudo apt-get -y install mysql-server mysql-client
+
+sudo apt-get -y install aptitude
+
+# Install PHP
+sudo aptitude -y install php5-fpm php5-suhosin php5-gd php-apc php5-mcrypt php5-cli php5-curl php5-mysql
+
+# Install memcached
+sudo aptitude -y install memcached php5-memcache
+
+# Install nginx
+sudo apt-get -y install nginx
